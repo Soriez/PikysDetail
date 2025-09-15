9864cb0c5eabdbe737deba11890d6e239d047f0a
Soriez <oriezsantiagogabriel@gmail.com>
2025-09-15 18:00:27 -0300

    implemetacion de menu hamburguesa v-mobile

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+const hamburguesa = document.querySelector('.hamburguesa')
+const nav = document.querySelector('nav')
+
+hamburguesa.addEventListener(
+    'click',
+    ()=>
+    {
+        nav.classList.toggle('show')
+    }
+)
